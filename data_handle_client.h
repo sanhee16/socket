3a4f5cf80fb77871c4ad14b48087a636629730f5
sanhee16 <sinhioa20@gmail.com>
2019-04-27 17:18:05 +0900

    client

--- data_handle_client.h
+++ data_handle_client.h
@@ -4,10 +4,25 @@ pthread_t real_cli_sndthread;
 static void *real_cli_rcvhandle(void * arg);
 static void *real_cli_sndhandle(void * arg);
 
-int server_ip="220.149.244.211";
+char* server_ip="220.149.244.211";
 
-pthread_mutex_t cli_lock;
-pthread_cond_t cli_cond;
+pthread_mutex_t cli_data_lock;
+
+typedef struct msg_data{
+    char snd_ip[15];
+    char recv_ip[15];
+    int snd_port;
+    int recv_port;
+    char msg[362];
+    // this structure size is 400
+}MSG_T;
+
+typedef struct buf{
+    MSG_T recv_buf;
+    int cli_sockfd;
+}BUF;
+
+
 
 static void * real_client_handle(void * arg){
 	int srv_sock, cli_sock;
@@ -15,7 +30,7 @@ static void * real_client_handle(void * arg){
 	struct sockaddr_in addr;
 	int len;
 
-	data_router_num=0;
+	int data_router_num=0;
 	port_num = 4712;
 
 	srv_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -48,7 +63,7 @@ static void * real_client_handle(void * arg){
 		return 0;
 	}
 	int cli_acc = accept(srv_sock, (struct sockaddr *)NULL, NULL);
-	int ret = -1;
+	ret = -1;
 	char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
 	/* get peer addr */
 	struct sockaddr peer_addr;
@@ -56,7 +71,7 @@ static void * real_client_handle(void * arg){
 	socklen_t peer_addr_len;
 	memset(&peer_addr, 0, sizeof(peer_addr));
 	peer_addr_len = sizeof(peer_addr);
-	ret = getpeername(cli_sockarr[a], &peer_addr, &peer_addr_len);
+	ret = getpeername(cli_acc, &peer_addr, &peer_addr_len);
 	ret = getnameinfo(&peer_addr, peer_addr_len,
 			hbuf, sizeof(hbuf), sbuf, sizeof(sbuf),
 			NI_NUMERICHOST | NI_NUMERICSERV);
@@ -71,8 +86,8 @@ static void * real_client_handle(void * arg){
 		perror("cli_sock connect ACCEPT fail");
 		close(srv_sock);
 	}
-	pthread_create(&real_srv_rcvthread,NULL,real_srv_rcvhandle,&cli_acc);
-	pthread_create(&real_srv_sndthread,NULL,real_srv_sndhandle,&cli_acc);
+	pthread_create(&real_cli_rcvthread,NULL,real_cli_rcvhandle,&cli_acc);
+	pthread_create(&real_cli_sndthread,NULL,real_cli_sndhandle,&cli_acc);
 	while(1){
 	}
 }
@@ -84,7 +99,7 @@ static void * real_cli_rcvhandle(void *arg){
 	int done=0;
 	
 	while(1){
-		pthread_mutex_lock(&cli_lock);
+		pthread_mutex_lock(&cli_data_lock);
 		MSG_T get_msg;
 		memset(&(get_msg),0,sizeof(get_msg));
 		int len;
@@ -103,7 +118,7 @@ static void * real_cli_rcvhandle(void *arg){
 
 		printf("%s",get_msg.msg);
 		fflush(NULL);
-		pthread_mutex_unlock(&cli_lock);
+		pthread_mutex_unlock(&cli_data_lock);
 	//	pthread_mutex_lock(&data_lock);
 	//	pthread_mutex_unlock(&data_lock);
 	}
@@ -118,10 +133,10 @@ static void * real_cli_sndhandle(void *arg){
 	int ret;
 	int done=0;
 	while(1){
-		pthread_mutex_lock(&data_lock);
+		pthread_mutex_lock(&cli_data_lock);
 
 			MSG_T snd_msg;
-			memset(snd_msg,0,sizeof(MSG_T));
+			memset(&snd_msg,0,sizeof(MSG_T));
 			//char* read_buffer = (char *)malloc(362);
 			//ret = read(1, read_buffer, 362);
 			ret = read(1, snd_msg.msg, 362);
@@ -134,19 +149,20 @@ static void * real_cli_sndhandle(void *arg){
 			int len = strlen(snd_msg.msg);
 			//int len = strlen(read_buffer);
 			if (len == 0) {
-				pthread_mutex_unlock(&cli_lock);
+				pthread_mutex_unlock(&cli_data_lock);
 				//free(read_buffer);
 				continue;
 			}
-
-			snd_msg.snd_ip="220.149.244.212";
-			snd_msg.recv_ip="220.149.244.211";
+			strcpy(snd_msg.snd_ip,"220.149.244.212");
+			strcpy(snd_msg.recv_ip,"220.149.244.211");
+			//snd_msg.snd_ip="220.149.244.212";
+			//snd_msg.recv_ip="220.149.244.211";
 			snd_msg.snd_port=4712;
 			snd_msg.recv_port=4712;
 
 			send(cli_sockfd,(char*)&snd_msg, sizeof(MSG_T), 0);
 		fflush(NULL);
-		pthread_mutex_unlock(&cli_lock);
+		pthread_mutex_unlock(&cli_data_lock);
 
 	}
 	while(1);
