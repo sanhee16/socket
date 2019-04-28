0411fcacedd3a9d67ed76cf44b7b8bab7346812d
sanhee16 <sinhioa20@gmail.com>
2019-04-28 17:31:58 +0900

    aa

--- newrouter.c
+++ newrouter.c
@@ -151,11 +151,9 @@ int main(int argc, char *argv[])
 	//pthread_create(&making_rr,NULL,RT_handler,NULL);
 
 
-	if(make_table==1){
 	pthread_create(&data_srv_thread,NULL,data_srv_handle,NULL);
 	if(my_num==0 || my_num==1 || my_num==2){
 		pthread_create(&cli_srv_connect_thread, NULL, data_srv_connect_handle, NULL);
-	}
 	}
 	while(1){
 	}
